4561d09df6e8498e2e6fdb12fbb959b02f4b2c33
Prajna Jeet Ojha <97689950+Prajna0106@users.noreply.github.com>
2025-11-02 12:26:40 +0530

    Created First Service

---
+++ first-service/src/main/java/com/demo/first_service/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.demo.first_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.GetExchange;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping("/test")
+    public String demo(){
+        return "First Service";
+    }
+}
